432da0aaa3dd9b9d91a4315bc877499c4d5644ef
Art San <artsan.mcc@gmail.com>
2024-09-15 23:27:50 +0700

    ok

--- backend/src/booking/booking.service.ts
+++ backend/src/booking/booking.service.ts
@@ -1,14 +1,198 @@
-import { Booking } from './entities/booking.entity'
+// import { Booking } from './entities/booking.entity'
+// import { Injectable, BadRequestException } from '@nestjs/common'
+// import { CreateBookingDto } from './dto/create-booking.dto'
+// import { UpdateBookingDto } from './dto/update-booking.dto'
+// import { DbService } from 'src/db/db.service'
+// import { BaseService } from 'src/common/base.service'
+// import {
+// 	parseInitData,
+// 	validateTelegramData,
+// } from 'src/utils/telegram-validation'
+// import { BotService } from 'src/both/bot.service'
+
+// @Injectable()
+// export class BookingService extends BaseService {
+// 	constructor(
+// 		private readonly dbService: DbService,
+// 		private readonly botService: BotService
+// 	) {
+// 		super(BookingService.name)
+// 	}
+
+// 	async findByIdBookLoc(bookingId: number) {
+// 		try {
+// 			const booking = await this.dbService.booking.findUnique({
+// 				where: { bookingId: bookingId },
+// 				include: {
+// 					doctor: true,
+// 					location: true,
+// 					diagnostic: true,
+// 				},
+// 			})
+
+// 			if (!booking) {
+// 				throw new BadRequestException(
+// 					`бронирование с таким ID: ${bookingId} нет в БД`
+// 				)
+// 			}
+
+// 			return booking
+// 			// return { msg: 'findByIdBookLoc' }
+// 		} catch (error) {
+// 			this.handleException(error, 'findByIdBookLoc booking')
+// 		}
+// 	}
+
+// 	async getBookingNotificationText(bookingId: number) {
+// 		console.log(454, 'bookingId', bookingId)
+// 		try {
+// 			const booking = await this.findByIdBookLoc(bookingId)
+
+// 			console.log(455, 'booking', booking)
+// 			let fieldDoc: string
+// 			if (booking?.doctor.doctorId) {
+// 				fieldDoc = `👨‍⚕️ Doctor: ${booking?.doctor.fullName}\n`
+// 			} else if (booking?.diagnosticId) {
+// 				fieldDoc = `🔬 Diagnostic: ${booking?.diagnostic.typeName}\n`
+// 			} else {
+// 				fieldDoc = 'чет не то'
+// 			}
+
+// 			const notificationText = `
+// 			🎉 Поздравляем! Ваше бронирование подтверждено. 🎉\n\n
+// 			📋 Запись №: ${booking.bookingId}\n
+// 			${fieldDoc}
+// 			📍 Локация: ${booking.location.name}: ${booking.location.address}\n\n
+// 			Спасибо, что выбрали наш сервис! Если у вас есть какие-либо вопросы или вам нужно перенести встречу, свяжитесь с нами 📞.
+// 			`
+
+// 			return notificationText
+// 			// return booking
+// 		} catch (error) {
+// 			this.handleException(error, 'getBookingNotificationText booking')
+// 		}
+// 	}
+// 	async creationSlotDoc(dto: CreateBookingDto) {
+// 		try {
+// 			// Validate Telegram data
+// 			const initData = dto.userInitData
+// 			// if (initData && !validateTelegramData(initData)) {
+// 			// 	throw new BadRequestException('Неверные инициализационные данные')
+// 			// }
+
+// 			// Разобрать исходные данные
+// 			// const parsedData = parseInitData(initData)
+
+// 			console.log(453, 'slot', dto)
+// 			// Вариант для размышления
+
+// 			const isSlot = await this.dbService.slot.findFirst({
+// 				where: {
+// 					startTime: dto.bookingDateTime,
+// 					diagnosticId: dto.diagnosticId,
+// 					doctorId: dto.doctorId,
+// 					locationId: dto.locationId,
+// 				},
+// 			})
+
+// 			console.log(454, 'slot', isSlot)
+
+// 			const isSlotBook = await this.dbService.booking.findFirst({
+// 				where: {
+// 					bookingDateTime: dto.bookingDateTime,
+// 					diagnosticId: dto.diagnosticId,
+// 					doctorId: dto.doctorId,
+// 					locationId: dto.locationId,
+// 				},
+// 			})
+// 			console.log(455, 'slotBook', isSlotBook)
+
+// 			const date = new Date(dto.bookingDateTime)
+// 			const hours = String(date.getHours()).padStart(2, '0')
+// 			const minutes = String(date.getMinutes()).padStart(2, '0')
+// 			const mount = date.getMonth()
+
+// 			if (isSlotBook || isSlot) {
+// 				throw new BadRequestException(
+// 					`Время приема ${hours}:${minutes} у этого специалиста не доступно`
+// 				)
+// 			}
+
+// 			const slot = await this.dbService.slot.create({
+// 				data: {
+// 					doctorId: dto.doctorId,
+// 					diagnosticId: dto.diagnosticId,
+// 					locationId: dto.locationId,
+// 					startTime: dto.bookingDateTime,
+// 					monthNumber: mount,
+// 				},
+// 			})
+
+// 			// Создайте запись о бронировании в базе данных
+// 			const booking = await this.dbService.booking.create({
+// 				data: {
+// 					telegramId: dto.telegramId,
+// 					userName: dto.userName,
+// 					userSurname: dto.userSurname,
+// 					userPhoneNumber: dto.userPhoneNumber,
+// 					userEmail: dto.userEmail,
+// 					userMessage: dto.userMessage,
+// 					bookingDateTime: dto.bookingDateTime,
+// 					doctorId: dto.doctorId,
+// 					diagnosticId: dto.diagnosticId,
+// 					locationId: dto.locationId,
+// 					// slotId: slot.slotId,
+// 				},
+// 			})
+
+// 			// При желании отправить уведомление пользователю
+// 			// if (parsedData && parsedData.user) {
+// 			// 	const userId = JSON.parse(parsedData.user).id
+// 			// 	// Предполагаем, что sendMessage — это метод отправки сообщения через Telegram
+// 			// 	await this.sendMessage(userId, 'Бронирование подтверждено')
+// 			// }
+
+// 			// const fieldDoc = await this.getBookingNotificationText(booking.bookingId)
+// 			// if (fieldDoc) {
+// 			// 	await this.botService.sendMessage(booking.telegramId, fieldDoc)
+// 			// }
+// 			// await this.sendMessage(booking.telegramId, 'Бронирование подтверждено')
+
+// 			// return { msg: 'cnm' }
+// 			return booking
+// 			// return booking
+// 		} catch (error) {
+// 			this.handleException(error, 'creationSlotDoc bookings')
+// 			// throw new BadRequestException('Не удалось создать бронирование')
+// 		}
+// 	}
+
+// 	private async sendMessage(userId: string, fieldDoc: string) {
+// 		// Реализация для отправки сообщения через Telegram
+// 		// Убедитесь, что идентификатор пользователя и текст имеют правильный тип (строка)
+// 	}
+
+// 	async findAll() {
+// 		try {
+// 			const booking = await this.dbService.booking.findMany({
+// 				// include: {
+// 				// 	specialty: true,
+// 				// 	location: true,
+// 				// },
+// 			})
+// 			return booking
+// 		} catch (error) {
+// 			this.handleException(error, 'findAll bookings')
+// 		}
+// 	}
+// }
+
 import { Injectable, BadRequestException } from '@nestjs/common'
 import { CreateBookingDto } from './dto/create-booking.dto'
-import { UpdateBookingDto } from './dto/update-booking.dto'
 import { DbService } from 'src/db/db.service'
 import { BaseService } from 'src/common/base.service'
-import {
-	parseInitData,
-	validateTelegramData,
-} from 'src/utils/telegram-validation'
 import { BotService } from 'src/both/bot.service'
+import { Prisma } from '@prisma/client'
 
 @Injectable()
 export class BookingService extends BaseService {
@@ -32,134 +216,111 @@ export class BookingService extends BaseService {
 
 			if (!booking) {
 				throw new BadRequestException(
-					`бронирование с таким ID: ${bookingId} нет в БД`
+					`Бронирование с таким ID: ${bookingId} не найдено в БД`
 				)
 			}
 
 			return booking
-			// return { msg: 'findByIdBookLoc' }
 		} catch (error) {
 			this.handleException(error, 'findByIdBookLoc booking')
+			throw error // Пробрасываем исключение дальше
 		}
 	}
 
 	async getBookingNotificationText(bookingId: number) {
-		console.log(454, 'bookingId', bookingId)
+		console.log('Booking ID:', bookingId)
 		try {
 			const booking = await this.findByIdBookLoc(bookingId)
 
-			console.log(455, 'booking', booking)
+			console.log('Booking:', booking)
 			let fieldDoc: string
-			if (booking?.doctor.doctorId) {
-				fieldDoc = `👨‍⚕️ Doctor: ${booking?.doctor.fullName}\n`
+			if (booking?.doctor?.doctorId) {
+				fieldDoc = `👨‍⚕️ Доктор: ${booking.doctor.fullName}\n`
 			} else if (booking?.diagnosticId) {
-				fieldDoc = `🔬 Diagnostic: ${booking?.diagnostic.typeName}\n`
+				fieldDoc = `🔬 Диагностика: ${booking.diagnostic.typeName}\n`
 			} else {
-				fieldDoc = 'чет не то'
+				fieldDoc = 'Ошибка данных'
 			}
 
 			const notificationText = `
-			🎉 Поздравляем! Ваше бронирование подтверждено. 🎉\n\n
-			📋 Запись №: ${booking.bookingId}\n
-			${fieldDoc}
-			📍 Локация: ${booking.location.name}: ${booking.location.address}\n\n
-			Спасибо, что выбрали наш сервис! Если у вас есть какие-либо вопросы или вам нужно перенести встречу, свяжитесь с нами 📞.
-			`
+        🎉 Поздравляем! Ваше бронирование подтверждено. 🎉\n\n
+        📋 Запись №: ${booking.bookingId}\n
+        ${fieldDoc}
+        📍 Локация: ${booking.location.name}: ${booking.location.address}\n\n
+        Спасибо, что выбрали наш сервис! Если у вас есть какие-либо вопросы или вам нужно перенести встречу, свяжитесь с нами 📞.
+      `
 
 			return notificationText
-			// return booking
 		} catch (error) {
 			this.handleException(error, 'getBookingNotificationText booking')
+			throw error // Пробрасываем исключение дальше
 		}
 	}
+
 	async creationSlotDoc(dto: CreateBookingDto) {
 		try {
-			// Validate Telegram data
 			const initData = dto.userInitData
-			// if (initData && !validateTelegramData(initData)) {
-			// 	throw new BadRequestException('Неверные инициализационные данные')
-			// }
 
-			// Разобрать исходные данные
-			// const parsedData = parseInitData(initData)
+			const existingSlot = await this.dbService.$transaction(
+				async (prisma: Prisma.TransactionClient) => {
+					const isSlot = await prisma.slot.findFirst({
+						where: {
+							startTime: dto.bookingDateTime,
+							diagnosticId: dto.diagnosticId,
+							doctorId: dto.doctorId,
+							locationId: dto.locationId,
+						},
+					})
 
-			console.log(453, 'slot', dto)
-			// Вариант для размышления
+					if (isSlot) {
+						throw new BadRequestException(
+							`Время приема ${dto.bookingDateTime} у этого специалиста не доступно`
+						)
+					}
 
-			const slot = await this.dbService.slot.findFirst({
-				where: {
-					startTime: dto.bookingDateTime,
-					diagnosticId: dto.diagnosticId,
-					doctorId: dto.doctorId,
-					locationId: dto.locationId,
-				},
-			})
+					const date = new Date(dto.bookingDateTime)
+					const hours = String(date.getHours()).padStart(2, '0')
+					const minutes = String(date.getMinutes()).padStart(2, '0')
+					const day = date.getDate()
+					const mount = date.getMonth()
 
-			console.log(454, 'slot', slot)
+					console.log(33, 'day', day)
+					console.log(34, 'day', new Date(dto.bookingDateTime).getDay())
+					const slot = await prisma.slot.create({
+						data: {
+							doctorId: dto.doctorId,
+							diagnosticId: dto.diagnosticId,
+							locationId: dto.locationId,
+							startTime: dto.bookingDateTime,
+							dayNumber: new Date(dto.bookingDateTime).getDay(),
+							monthNumber: new Date(dto.bookingDateTime).getMonth(),
+						},
+					})
 
-			const slotBook = await this.dbService.booking.findFirst({
-				where: {
-					locationId: dto.locationId,
-					bookingDateTime: dto.bookingDateTime,
-				},
-			})
-			console.log(455, 'slotBook', slotBook)
+					const booking = await prisma.booking.create({
+						data: {
+							telegramId: dto.telegramId,
+							userName: dto.userName,
+							userSurname: dto.userSurname,
+							userPhoneNumber: dto.userPhoneNumber,
+							userEmail: dto.userEmail,
+							userMessage: dto.userMessage,
+							bookingDateTime: dto.bookingDateTime,
+							doctorId: dto.doctorId,
+							diagnosticId: dto.diagnosticId,
+							locationId: dto.locationId,
+							slotId: slot.slotId,
+						},
+					})
 
-			const date = new Date(dto.bookingDateTime)
-			const hours = String(date.getHours()).padStart(2, '0')
-			const minutes = String(date.getMinutes()).padStart(2, '0')
-			const mount = date.getMonth()
+					return booking
+				}
+			)
 
-			if (slotBook || slot) {
-				throw new BadRequestException(
-					`Время приема ${hours}:${minutes} у этого специалиста не доступно`
-				)
-			}
-
-			// await this.dbService.slot.create({
-			// 	data: {
-			// 		doctorId: dto.doctorId,
-			// 		locationId: dto.locationId,
-			// 		startTime: dto.bookingDateTime,
-			// 		monthNumber: mount,
-			// 	},
-			// })
-
-			// Создайте запись о бронировании в базе данных
-			// const booking = await this.dbService.booking.create({
-			// 	data: {
-			// 		telegramId: dto.telegramId,
-			// 		userName: dto.userName,
-			// 		userSurname: dto.userSurname,
-			// 		userPhoneNumber: dto.userPhoneNumber,
-			// 		userEmail: dto.userEmail,
-			// 		userMessage: dto.userMessage,
-			// 		bookingDateTime: dto.bookingDateTime,
-			// 		doctorId: dto.doctorId,
-			// 		diagnosticId: dto.diagnosticId,
-			// 		locationId: dto.locationId,
-			// 	},
-			// })
-
-			// При желании отправить уведомление пользователю
-			// if (parsedData && parsedData.user) {
-			// 	const userId = JSON.parse(parsedData.user).id
-			// 	// Предполагаем, что sendMessage — это метод отправки сообщения через Telegram
-			// 	await this.sendMessage(userId, 'Бронирование подтверждено')
-			// }
-
-			// const fieldDoc = await this.getBookingNotificationText(booking.bookingId)
-			// if (fieldDoc) {
-			// 	await this.botService.sendMessage(booking.telegramId, fieldDoc)
-			// }
-			// await this.sendMessage(booking.telegramId, 'Бронирование подтверждено')
-
-			return { msg: 'cnm' }
-			// return booking
-			// return booking
+			return existingSlot
 		} catch (error) {
 			this.handleException(error, 'creationSlotDoc bookings')
-			// throw new BadRequestException('Не удалось создать бронирование')
+			throw error // Пробрасываем исключение дальше
 		}
 	}
 
@@ -172,13 +333,14 @@ export class BookingService extends BaseService {
 		try {
 			const booking = await this.dbService.booking.findMany({
 				// include: {
-				// 	specialty: true,
-				// 	location: true,
+				// specialty: true,
+				// location: true,
 				// },
 			})
 			return booking
 		} catch (error) {
 			this.handleException(error, 'findAll bookings')
+			throw error // Пробрасываем исключение дальше
 		}
 	}
 }
--- backend/src/booking/test.ts
+++ backend/src/booking/test.ts
@@ -1,97 +1,207 @@
-// 1. Контроллер (Controller)
-/////////////////////////////////////////////////////////////////////////////////
-// import { Body, Controller, Post, BadRequestException } from '@nestjs/common';
-// import { DoctorService } from './doctor.service';
-// import { CreateBookingDto } from './dto/create-booking.dto';
-// import { TelegramService } from './telegram.service'; // сервис для отправки сообщения в Telegram
+// model Slot {
+//   slotId       Int      @id @default(autoincrement())
+//   doctorId     Int?     // Слот может быть привязан к доктору
+//   diagnosticId Int?     // Или к диагностике
+//   locationId   Int      // И обязательно привязан к локации
+//   startTime    DateTime  // Время начала слота в формате ISO 8601
+//   endTime      DateTime  // Время окончания слота в формате ISO 8601
+//   dayNumber    Int      // Сделать поле обязательным
+//   monthNumber  Int      // Номер месяца (1-12) для фильтрации
+//   isBooked     Boolean  @default(false) // Флаг занятости слота
+//   createdAt    DateTime @default(now())
+//   updatedAt    DateTime @updatedAt
 
-// @Controller('doctor')
-// export class DoctorController {
+//   doctor       Doctor?     @relation(fields: [doctorId], references: [doctorId])
+//   diagnostic   Diagnostic? @relation(fields: [diagnosticId], references: [diagnosticId])
+//   location     Location    @relation(fields: [locationId], references: [locationId])
+//   booking      Booking?    // Ссылка на бронирование, если слот занят
+
+//   @@map("slots")
+// }
+
+// model Booking {
+//   bookingId        Int      @id @default(autoincrement())
+//   userId           Int?
+//   telegramId       String?
+//   userName         String
+//   userSurname      String
+//   userPhoneNumber  String
+//   userEmail        String?
+//   userMessage      String?
+//   bookingDateTime  DateTime @db.Timestamptz
+//   doctorId         Int?
+//   diagnosticId     Int?
+//   locationId       Int
+//   slotId           Int?     // Убрать уникальность
+//   status           BookingStatus @default(PENDING)
+//   createdAt        DateTime @default(now())
+//   updatedAt        DateTime @updatedAt
+//   deletedAt        DateTime?
+
+//   user             User?     @relation(fields: [userId], references: [userId])
+//   doctor           Doctor?   @relation(fields: [doctorId], references: [doctorId])
+//   diagnostic       Diagnostic? @relation(fields: [diagnosticId], references: [diagnosticId])
+//   location         Location  @relation(fields: [locationId], references: [locationId])
+//   slot             Slot?    @relation(fields: [slotId], references: [slotId])
+//   results          DiagnosticResult[]
+
+//   @@unique([doctorId, locationId, bookingDateTime], name: "unique_doctor_location_time")
+//   @@unique([diagnosticId, locationId, bookingDateTime], name: "unique_diagnostic_location_time")
+
+//   @@map("bookings")
+// }
+
+// enum BookingStatus {
+//   PENDING
+//   FINISHED
+//   CANCELLED
+// }
+
+// import { Injectable, BadRequestException } from '@nestjs/common'
+// import { CreateBookingDto } from './dto/create-booking.dto'
+// import { DbService } from 'src/db/db.service'
+// import { BaseService } from 'src/common/base.service'
+// import { BotService } from 'src/both/bot.service'
+
+// @Injectable()
+// export class BookingService extends BaseService {
 // 	constructor(
-// 		private readonly doctorService: DoctorService
-// 		// private readonly telegramService: TelegramService, // для отправки уведомлений в Telegram
-// 	) {}
-
-// 	@Post('book_slot')
-// 	async bookSlot(@Body() createBookingDto: CreateBookingDto) {
-// 		const initData = createBookingDto.userInitData
-
-// 		if (initData && !this.doctorService.validateTelegramData(initData)) {
-// 			throw new BadRequestException('Invalid initData')
-// 		}
-
-// 		const parsedData = this.doctorService.parseInitData(initData)
-// 		const result = await this.doctorService.bookSlot(
-// 			createBookingDto,
-// 			parsedData
-// 		)
-
-// 		if (initData) {
-// 			const userInfo = parsedData?.user
-// 			if (userInfo) {
-// 				const userId = JSON.parse(userInfo).id
-// 				const notification =
-// 					await this.doctorService.getBookingNotificationText(result.booking_id)
-// 				await this.telegramService.sendMessage(userId, notification)
-// 			}
-// 		}
-
-// 		return result
+// 		private readonly dbService: DbService,
+// 		private readonly botService: BotService
+// 	) {
+// 		super(BookingService.name)
 // 	}
-// }
 
-// 2. Сервис (Service)
-/////////////////////////////////////////////////////////////////////////////////////////////
-// import { Injectable } from '@nestjs/common';
-// import { CreateBookingDto } from './dto/create-booking.dto';
+// 	async findByIdBookLoc(bookingId: number) {
+// 		try {
+// 			const booking = await this.dbService.booking.findUnique({
+// 				where: { bookingId: bookingId },
+// 				include: {
+// 					doctor: true,
+// 					location: true,
+// 					diagnostic: true,
+// 				},
+// 			})
 
-// @Injectable()
-// export class DoctorService {
-//   async bookSlot(createBookingDto: CreateBookingDto, parsedData: any) {
-//     // Логика бронирования слота, например, взаимодействие с базой данных
-//     const bookingResult = {
-//       booking_id: 123, // предположим, что бронирование успешно и возвращается ID
-//     };
-//     return bookingResult;
-//   }
+// 			if (!booking) {
+// 				throw new BadRequestException(
+// 					`Бронирование с таким ID: ${bookingId} не найдено в БД`
+// 				)
+// 			}
 
-//   validateTelegramData(initData: string): boolean {
-//     // Логика проверки данных из Telegram
-//     return true; // верните false, если проверка не прошла
-//   }
+// 			return booking
+// 		} catch (error) {
+// 			this.handleException(error, 'findByIdBookLoc booking')
+// 		}
+// 	}
 
-//   parseInitData(initData: string): any {
-//     // Логика парсинга данных из Telegram
-//     return { user: JSON.stringify({ id: '123456789' }) }; // пример возвращаемых данных
-//   }
+// 	async getBookingNotificationText(bookingId: number) {
+// 		console.log('Booking ID:', bookingId)
+// 		try {
+// 			const booking = await this.findByIdBookLoc(bookingId)
 
-//   async getBookingNotificationText(bookingId: number): Promise<string> {
-//     // Логика получения текста уведомления
-//     return `Your booking is confirmed with ID ${bookingId}`;
-//   }
-// }
+// 			console.log('Booking:', booking)
+// 			let fieldDoc: string
+// 			if (booking?.doctor?.doctorId) {
+// 				fieldDoc = `👨‍⚕️ Доктор: ${booking.doctor.fullName}\n`
+// 			} else if (booking?.diagnosticId) {
+// 				fieldDoc = `🔬 Диагностика: ${booking.diagnostic.typeName}\n`
+// 			} else {
+// 				fieldDoc = 'Ошибка данных'
+// 			}
 
-//  4. Модуль (Module)
-//////////////////////////////////////////////////////////////////////////
-// import { Module } from '@nestjs/common';
-// import { DoctorController } from './doctor.controller';
-// import { DoctorService } from './doctor.service';
-// import { TelegramService } from './telegram.service'; // добавьте TelegramService
+// 			const notificationText = `
+//       🎉 Поздравляем! Ваше бронирование подтверждено. 🎉\n\n
+//       📋 Запись №: ${booking.bookingId}\n
+//       ${fieldDoc}
+//       📍 Локация: ${booking.location.name}: ${booking.location.address}\n\n
+//         Спасибо, что выбрали наш сервис! Если у вас есть какие-либо вопросы или вам нужно перенести встречу, свяжитесь с нами 📞.
+//       `
 
-// @Module({
-//   controllers: [DoctorController],
-//   providers: [DoctorService, TelegramService], // добавьте TelegramService
-// })
-// export class DoctorModule {}
+// 			return notificationText
+// 		} catch (error) {
+// 			this.handleException(error, 'getBookingNotificationText booking')
+// 		}
+// 	}
 
-// 5. TelegramService
-///////////////////////////////////////////////////////////////////
-// import { Injectable } from '@nestjs/common';
+// 	async creationSlotDoc(dto: CreateBookingDto) {
+// 		try {
+// 			const initData = dto.userInitData
 
-// @Injectable()
-// export class TelegramService {
-//   async sendMessage(chatId: string, text: string) {
-//     // Логика отправки сообщения через Telegram Bot API
-//     console.log(`Sending message to ${chatId}: ${text}`);
-//   }
+// 			const isSlot = await this.dbService.slot.findFirst({
+// 				where: {
+// 					startTime: dto.bookingDateTime,
+// 					diagnosticId: dto.diagnosticId,
+// 					doctorId: dto.doctorId,
+// 					locationId: dto.locationId,
+// 				},
+// 			})
+
+// 			const date = new Date(dto.bookingDateTime)
+// 			const hours = String(date.getHours()).padStart(2, '0')
+// 			const minutes = String(date.getMinutes()).padStart(2, '0')
+// 			const mount = date.getMonth()
+
+// 			if (isSlot) {
+// 				throw new BadRequestException(
+// 					`Время записи ${hours}:${minutes} не доступно`
+// 				)
+// 			}
+
+// 			const slot = await this.dbService.slot.create({
+// 				data: {
+// 					doctorId: dto.doctorId,
+// 					diagnosticId: dto.diagnosticId,
+// 					locationId: dto.locationId,
+// 					startTime: dto.bookingDateTime,
+// 					monthNumber: new Date(dto.bookingDateTime).getMonth() + 1,
+// 				},
+// 			})
+
+// 			const booking = await this.dbService.booking.create({
+// 				data: {
+// 					telegramId: dto.telegramId,
+// 					userName: dto.userName,
+// 					userSurname: dto.userSurname,
+// 					userPhoneNumber: dto.userPhoneNumber,
+// 					userEmail: dto.userEmail,
+// 					userMessage: dto.userMessage,
+// 					bookingDateTime: dto.bookingDateTime,
+// 					doctorId: dto.doctorId,
+// 					diagnosticId: dto.diagnosticId,
+// 					locationId: dto.locationId,
+// 					slotId: slot.slotId,
+// 				},
+// 			})
+
+// 			const fieldDoc = await this.getBookingNotificationText(booking.bookingId)
+// 			if (fieldDoc) {
+// 				await this.botService.sendMessage(booking.telegramId, fieldDoc)
+// 			}
+// 			await this.sendMessage(booking.telegramId, 'Бронирование подтверждено')
+
+// 			return booking
+// 		} catch (error) {
+// 			this.handleException(error, 'creationSlotDoc bookings')
+// 		}
+// 	}
+
+// 	private async sendMessage(userId: string, fieldDoc: string) {
+// 		// Реализация для отправки сообщения через Telegram
+// 		// Убедитесь, что идентификатор пользователя и текст имеют правильный тип (строка)
+// 	}
+
+// 	async findAll() {
+// 		try {
+// 			const booking = await this.dbService.booking.findMany({
+// 				// include: {
+// 				//  specialty: true,
+// 				//  location: true,
+// 				// },
+// 			})
+// 			return booking
+// 		} catch (error) {
+// 			this.handleException(error, 'findAll bookings')
+// 		}
+// 	}
 // }
